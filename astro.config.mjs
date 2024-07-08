adb60ff300b256f57c4ee3a56b7ca5afa6c9b1ec
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-07-08 10:30:52 +0200

    Remove all

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-i18n-starter.pages.dev',
-  integrations: [mdx(), sitemap(), vue()],
-  markdown: {}
+  integrations: [mdx(), vue()]
 });

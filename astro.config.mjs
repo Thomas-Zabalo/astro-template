8c8e2c8017ff02d4974933a54592ac195b581d6d
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-07-08 10:36:42 +0200

    Add reset CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
-import vue from "@astrojs/vue";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), vue()]
+  integrations: [mdx()]
 });

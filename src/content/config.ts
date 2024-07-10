aaf2f8e1ebfde65d21eabe3ef218ff8695e79277
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-07-10 22:30:48 +0200

    add mdx

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,21 @@
 import { z, defineCollection } from "astro:content";
 
 const projectsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      date: z.string(),
-      image: z.object({
-        src: z.string(),
-        alt: z.string()
-      }),
-    })
+  type: 'content',
+  schema: z.object({
+    author: z.object({
+      name: z.string(),
+      img: z.string(),
+      alt: z.string(),
+    }),
+    title: z.string(),
+    description: z.string(),
+    date: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string().optional()
+    }),
+  })
 });
 
 export const collections = {

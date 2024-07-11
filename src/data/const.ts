50576e5c1ed4a6573af5a82180a0417ea4453a3f
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-07-11 14:20:52 +0200

    fix light theme

--- src/data/const.ts
+++ src/data/const.ts
@@ -20,35 +20,16 @@ export type Hero = {
     actions?: Link[];
 };
 
-export type Subscribe = {
-    title?: string;
-    text?: string;
-    formUrl: string;
-};
 
 export type SiteConfig = {
-    logo?: Image;
-    title: string;
-    subtitle?: string;
-    description: string;
     image?: Image;
     headerNavLinks?: Link[];
     footerNavLinks?: Link[];
     socialLinks?: Link[];
     hero?: Hero;
-    subscribe?: Subscribe;
-    postsPerPage?: number;
-    projectsPerPage?: number;
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
-    image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
-    },
     headerNavLinks: [
         {
             text: 'Projects',
@@ -65,6 +46,10 @@ const siteConfig: SiteConfig = {
             href: '/about'
         },
         {
+            text: 'Markdown style',
+            href: '/markdown'
+        },
+        {
             text: 'Contact',
             href: '/contact'
         },
@@ -74,7 +59,7 @@ const siteConfig: SiteConfig = {
         },
         {
             text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
+            href: 'https://github.com/Thomas-Zabalo/astro-template'
         }
     ],
     socialLinks: [
@@ -92,11 +77,12 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Showcasing My Work: A Journey in Web Development',
+        text: "Hello, I'm Emily Johnson, a passionate web developer driven by creativity and a love for crafting exceptional digital experiences. My journey in web development is marked by a deep curiosity for cutting-edge technologies and a commitment to delivering innovative solutions. I thrive in dynamic environments where I merge creativity with technical expertise to build impactful websites and applications. With a sharp eye for detail and a dedication to user-centric design, I aim to enhance user experiences through intuitive interfaces and robust backend solutions.",
         image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            src: '/hero.jpg',
+            alt: 'A person sitting at a desk in front of a computer at work',
+            caption: '',
         },
         actions: [
             {
